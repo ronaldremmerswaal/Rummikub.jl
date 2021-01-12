f96e6df559aef096e54350e1094dbfaea51eda89
Ronald Remmerswaal <r.a.remmerswaal@rug.nl>
2021-01-12 22:53:26 +0100

    Use MMatrix

--- src/Rummikub.jl
+++ src/Rummikub.jl
@@ -15,24 +15,25 @@ using StaticArrays
 # S minimum run and group length
 struct GameState{N,K,M}
   # Available tiles (hand + table)
-  available::SizedArray{Tuple{N,K}, Int}
+  available::MArray{Tuple{N,K}, Int}
 
   # Current runs that are formed (true iff tile is used)
-  runs::SizedArray{Tuple{N,K,M}, Bool}
+  runs::MArray{Tuple{N,K,M}, Bool}
 
   # Tiles that are on the table
-  table::SizedArray{Tuple{N,K}, Int}
+  table::MArray{Tuple{N,K}, Int}
 
-  GameState{N, K, M}() where {N, K, M} = new(SizedArray{Tuple{N,K}, Int}(zeros(Int, N, K)), SizedArray{Tuple{N,K,M}, Bool}(zeros(Bool, N, K, M)), SizedArray{Tuple{N,K}, Int}(zeros(Int, N, K)))
-  GameState{N, K, M}(available::SizedArray{Tuple{N,K}, Int}, runs::SizedArray{Tuple{N,K,M}, Bool}, table::SizedArray{Tuple{N,K}, Int}) where {N, K, M} = new(available, runs, table)
+  GameState{N, K, M}() where {N, K, M} = new(MArray{Tuple{N,K}, Int}(zeros(Int, N, K)), MArray{Tuple{N,K,M}, Bool}(zeros(Bool, N, K, M)), MArray{Tuple{N,K}, Int}(zeros(Int, N, K)))
+  GameState{N, K, M}(available::MArray{Tuple{N,K}, Int}, runs::MArray{Tuple{N,K,M}, Bool}, table::MArray{Tuple{N,K}, Int}) where {N, K, M} = new(available, runs, table)
 end
 
-function GameState(hand::SizedArray{Tuple{N,K}, Int}, table::SizedArray{Tuple{N,K}, Int}) where {N, K}
+function GameState(hand::MArray{Tuple{N,K}, Int}, table::MArray{Tuple{N,K}, Int}) where {N, K}
   available = hand + table
-  M = min(1, max(available))
-  return GameState(hand + table, SizedArray{Tuple{N,K,M}, Bool}(zeros(Bool, N, K, M)), table)
+  M = max(1, maximum(available))
+  return GameState{N, K, M}(available, MArray{Tuple{N,K,M}, Bool}(zeros(Bool, N, K, M)), table)
 end
 
+# TODO how large is third dimension of score/max_index? (less than 3^M)
 struct ScoreMemory{N,K,M}
   score::Array{Int}
   max_index::Array{Int}
@@ -40,12 +41,12 @@ end
 
 # Extend the runs by the given extension, and remove tiles from the available
 # and table array
-function extend_runs!(state::GameState{N,K,M}, extension, value::Int)
+function extend_runs!(state::GameState{N,K,M}, extension, value::Int) where {N,K,M}
 
 end
 
 # Undo the action of extend_runs!
-function undo_extend_runs!(state::GameState{N,K,M}, extension, value::Int)
+function undo_extend_runs!(state::GameState{N,K,M}, extension, value::Int) where {N,K,M}
 
 end
 
@@ -55,17 +56,17 @@ end
 #   = S-1: then the increment for this run will be value-S+1 + ... + value-1 + value
 #   > S-1: then the increment for this run will be value
 # So all (state, value) pairs that map to index have the same max_score_increment.
-function index(state::GameState{N,K,M}, value::Int)
+function index(state::GameState{N,K,M}, value::Int) where {N,K,M}
 
 end
 
 # The maximum score increment that can be achieved by adding all tiles ≥ value
 # that are available
-function max_score_increment(memory::ScoreMemory{N,K,M}, state::GameState{N}, value::Int)
-  if value > N return 0
+function max_score_increment(memory::ScoreMemory{N,K,M}, state::GameState{N}, value::Int) where {N,K,M}
+  if value > N return 0 end
   I = index(state, value)
   # Skip if score score increment has already been computed
-  if memory.score[I] > -Inf return memory.score[I]
+  if memory.score[I] > -Inf return memory.score[I] end
 
   # Consider all possible run extensions of the current state, using the
   # available tiles of the current value
@@ -92,14 +93,28 @@ function max_score_increment(memory::ScoreMemory{N,K,M}, state::GameState{N}, va
 end
 
 # The increment in score achieved by the use of value in state
-function score_increment(state::GameState{N}, value::Int)
-
+function score_increment(state::GameState{N,K,M}, value::Int)  where {N,K,M}
+  ans = 0
+  for suit = 1 : K
+    for deck = 1 : M
+      if state.runs[value,suit,deck]
+        if value > S && all(state.runs[value-S:value,suit,deck])
+          # Run was already valid
+          ans += value
+        elseif value > S - 1 && all(state.runs[value-S+1:value,suit,deck])
+          # Run becomes valid, so the increment is
+          #   value - S + 1, value - S + 2, ..., value
+          ans += Int(S * (value - (S - 1)/2))
+        end
+      end
+    end
+  end
 end
 
 # Form as large as possible valid groups with the available tiles of the current value
 # returns the sum of the group sizes
 # A valid group is one which has length at least S
-function sum_of_valid_group_sizes(state::GameState{N}, value::Int)
+function sum_of_valid_group_sizes(state::GameState{N,K,M}, value::Int) where {N,K,M}
 
 end
 
@@ -109,14 +124,13 @@ end
 # with a larger value
 # That is, runs which will require value+i further on, without any value+i being available
 # of that particular suit, are omitted
-function run_extensions(state::GameState{N}, value::Int)
+function run_extensions(state::GameState{N,K,M}, value::Int) where {N,K,M}
 
 end
 
-# Given the maximum score increments, reconstruct which runs and groups result in
-# this score
-function reconstruct_optimal_solution(memory::ScoreMemory{N,K,M})
-  state = GameState{N, K, M}()
+# Given the maximum score increments, reconstruct which runs result in
+# this score (groups trivially follow from state.available)
+function reconstruct_optimal_runs!(state::GameState{N, K, M}, memory::ScoreMemory{N,K,M}) where {N,K,M}
   value = 0
   I = index(state, value)
   while value ≤ N
@@ -126,17 +140,14 @@ function reconstruct_optimal_solution(memory::ScoreMemory{N,K,M})
 
     extend_runs!(state, extension, value)
 
-    # TODO should we store groups in the state as well?
-    form_groups!(state, value)
-
     value += 1
   end
 
-  return state.runs
+  return state
 end
 
 # Given a state and an extended index, find the corresponding extension
-function reconstruct_extension(state::GameState{N,K,M}, extended_index)
+function reconstruct_extension(state::GameState{N,K,M}, extended_index) where {N,K,M}
 
 end
 
